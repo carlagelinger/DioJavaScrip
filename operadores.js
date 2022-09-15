193e98311bcfd458d87ae93108bf12060b9ee98e
Carla <carlagelinger@gmail.com>
2022-09-15 10:11:03 -0300

    Adicionando repositório Curso Dio JavaScript ao GitHub

---
+++ operadores.js
@@ -0,0 +1,7 @@
+function comparaNumeros (num1,num2) {
+    const saoIguais = num1 === num2;
+    const soma = num1 + num2;
+
+    
+
+}
---
+++ questao1.js
@@ -0,0 +1,9 @@
+var String frase = "Comprei rapadura e salsa com entusiasmo rarrísimo"
+
+function solucao(frase) 
+{
+    //SEU CÓDIGO VAI AQUI
+    return frase.slice(start)
+  }
+  
+ 
